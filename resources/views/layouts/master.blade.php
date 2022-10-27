40f33e50d7afde59f1b0fa5e7b6e7c0c5f175150
Muhammad Irfan <m.irfan0205@gmail.com>
2022-10-27 16:27:18 +0700

    fix jquery

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -160,33 +160,32 @@
         <!-- ./wrapper -->
 
         <!-- jQuery -->
-        <script src="{{ asset('vendors../../plugins/jquery/jquery.min.js"') }}></script>
+        <script src="{{ asset('vendors/plugins/jquery/jquery.min.js') }}"></script>
         <!-- Bootstrap 4 -->
-        <script src="{{ asset('vendors../../plugins/bootstrap/js/bootstrap.bundle.min.js"') }}></script>
+        <script src="{{ asset('vendors/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
         <!-- Select2 -->
-        <script src="{{ asset('vendors../../plugins/select2/js/select2.full.min.js"') }}></script>
+        <script src="{{ asset('vendors/plugins/select2/js/select2.full.min.js') }}"></script>
         <!-- Bootstrap4 Duallistbox -->
-        <script src="{{ asset('vendors../../plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"') }}></script>
+        <script src="{{ asset('vendors/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js') }}"></script>
         <!-- InputMask -->
-        <script src="{{ asset('vendors../../plugins/moment/moment.min.js"') }}></script>
-        <script src="{{ asset('vendors../../plugins/inputmask/jquery.inputmask.min.js"') }}></script>
+        <script src="{{ asset('vendors/plugins/moment/moment.min.js') }}"></script>
+        <script src="{{ asset('vendors/plugins/inputmask/jquery.inputmask.min.js') }}"></script>
         <!-- date-range-picker -->
-        <script src="{{ asset('vendors../../plugins/daterangepicker/daterangepicker.js"') }}></script>
+        <script src="{{ asset('vendors/plugins/daterangepicker/daterangepicker.js') }}"></script>
         <!-- bootstrap color picker -->
-        <script src="{{ asset('vendors../../plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"') }}></script>
+        <script src="{{ asset('vendors/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js') }}"></script>
         <!-- Tempusdominus Bootstrap 4 -->
-        <script
-            src="{{ asset('vendors../../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"') }}></script>
+        <script src="{{ asset('vendors/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
         <!-- Bootstrap Switch -->
-        <script src="{{ asset('vendors../../plugins/bootstrap-switch/js/bootstrap-switch.min.js"') }}></script>
+        <script src="{{ asset('vendors/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}"></script>
         <!-- BS-Stepper -->
-        <script src="{{ asset('vendors../../plugins/bs-stepper/js/bs-stepper.min.js"') }}></script>
+        <script src="{{ asset('vendors/plugins/bs-stepper/js/bs-stepper.min.js') }}"></script>
         <!-- dropzonejs -->
-        <script src="{{ asset('vendors../../plugins/dropzone/min/dropzone.min.js"') }}></script>
+        <script src="{{ asset('vendors/plugins/dropzone/min/dropzone.min.js') }}"></script>
         <!-- AdminLTE App -->
-        <script src="{{ asset('vendors../../dist/js/adminlte.min.js"') }}></script>
+        <script src="{{ asset('vendors/dist/js/adminlte.min.js') }}"></script>
         <!-- AdminLTE for demo purposes -->
-        <!-- <script src="{{ asset('vendors../../dist/js/demo.js"') }}></script> -->
+        <!-- <script src="{{ asset('vendors/dist/js/demo.js"') }}></script> -->
         <!-- Page specific script -->
 
         {{-- date --}}
